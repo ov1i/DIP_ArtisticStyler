d95005f0feb9ea749e4b810e017b8bd3f5e21760
Horvath Daiana <horvathdaiana@yahoo.com>
2024-10-07 00:20:52 +0300

    fixed converion errors

--- src/conversion/RgbToLab.py
+++ src/conversion/RgbToLab.py
@@ -1,52 +1,58 @@
 import numpy as np
+import cv2
 
 # Function to convert from RGB to LAB
 def rgb_to_lab(src):
    
-    r, g, b = src[:, :, 2], src[:, :, 1], src[:, :, 0]
+    r, g, b = src[:, :, 0], src[:, :, 1], src[:, :, 2]
 
-    # Normalize RGB
+
+    # # Normalize RGB
     r = r / 255.0
     g = g / 255.0
     b = b / 255.0
    
-    # Apply gamma correction
+     # Apply gamma correction
     def gamma_correction(c):
-        return c / 12.92 if c <= 0.04045 else ((c + 0.055) / 1.055) ** 2.4
+        return np.where(c <= 0.04045 , c/12.92, ((c + 0.055) / 1.055) ** 2.4)
     
-    height, width, channels = src.shape
-
-    for i in range(height):
-        for j in range(width):
-            r[i][j]=gamma_correction(r[i][j])
-            g[i][j]=gamma_correction(g[i][j])
-            b[i][j]=gamma_correction(b[i][j])
-
+    r=gamma_correction(r)
+    g=gamma_correction(g)
+    b=gamma_correction(b)
+    
    
-    # Convert RGB to XYZ
+     # Convert RGB to XYZ
     X = r * 0.4124564 + g * 0.3575761 + b * 0.1804375
     Y = r * 0.2126729 + g * 0.7151522 + b * 0.0721750
     Z = r * 0.0193339 + g * 0.1191920 + b * 0.9503041
 
-    # Normalize for the D65 illuminant
+
+
+     # Normalize for the D65 illuminant
     X /= 0.95047
     Z /= 1.08883
 
-    # Helper function for f(t)
-    def f(t):
-        return t ** (1/3) if t > 0.008856 else (7.787 * t + 16 / 116)
+    # # Normalize for the D50 illuminant
+    # #X /= 0.95047
+    # #Z /= 1.08883
+    # #TODO: to be added(check wiki)
 
-    X=np.asarray(X)
-    print(X)
-    # Convert XYZ to LAB
+    # # Helper function for f(t)
+    def f(t):
+        return np.where(t>0.008856, t**(1/3),(7.787 * t + 16 / 116 ))
+
+    
     L = 116 * f(Y) - 16
     a = 500 * (f(X) - f(Y))
     b = 200 * (f(Y) - f(Z))
 
-    src[:, :, 2], src[:, :, 1], src[:, :, 0]=L,a,b
-   
+    L = np.clip(L, 0, 100)
+    a = np.clip(a, -128, 127)
+    b = np.clip(b, -128, 127)
 
-    return src
+    lab_image = np.stack([L, a, b], axis=-1).astype(np.float32)
+
+    return lab_image
 
 
     
--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ import cv2
 import numpy as np
 import conversion.RgbToLab as cc
 img = cv2.imread("res/test.jpeg")
+img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 transf_img=cc.rgb_to_lab(img)
+
 cv2.imshow("test", transf_img)
 cv2.waitKey(0)
+
 
 
